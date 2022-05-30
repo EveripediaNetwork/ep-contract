1a0307a90653bf04a52b2af9c63265c87f492917
Yusuf Kehinde Hussein <yusufkehinde11@gmail.com>
2022-05-30 16:14:08 +0100

    feat: added editor nft contract and test

--- src/Editor/Editor.sol
+++ src/Editor/Editor.sol
@@ -1,1 +1,78 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.13;
 // transferable nft user needs this nft to create wikis
+
+import "../LilOwnable.sol";
+import "../Strings.sol";
+import "solmate/tokens/ERC721.sol";
+import "solmate/utils/SafeTransferLib.sol";
+import "solmate/auth/Owned.sol";
+
+error MintPriceNotPaid();
+error MaxSupply();
+error NonExistentTokenURI();
+error WithdrawTransfer();
+
+contract Editor is ERC721, Owned {
+    uint256 public TOTAL_SUPPLY = 9999;
+    uint256 public constant PRICE_PER_MINT = 0.08 ether;
+    uint256 public currentTokenId;
+    string public baseURI;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        string memory _baseURI
+    ) payable ERC721(name, symbol) Owned(msg.sender) {
+        baseURI = _baseURI;
+    }
+
+    // function mint(uint16 amount) external payable {
+    //     if (currentTokenId + amount >= TOTAL_SUPPLY) revert NoTokensLeft();
+    //     if (msg.value < amount * PRICE_PER_MINT) revert NotEnoughETH();
+
+    //     unchecked {
+    //         for (uint16 index = 0; index < amount; index++) {
+    //             _mint(msg.sender, currentTokenId++);
+    //         }
+    //     }
+    // }
+
+    function mintTo(address recipient) public payable returns (uint256) {
+        if (msg.value != PRICE_PER_MINT) {
+            revert MintPriceNotPaid();
+        }
+        uint256 newTokenId = ++currentTokenId;
+        if (newTokenId > TOTAL_SUPPLY) {
+            revert MaxSupply();
+        }
+        _safeMint(recipient, newTokenId);
+        return newTokenId;
+    }
+
+    function increaseTotalSupply(uint256 amount) public onlyOwner {
+        TOTAL_SUPPLY += amount;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        if (ownerOf(tokenId) == address(0)) revert NonExistentTokenURI();
+        return
+            bytes(baseURI).length > 0
+                ? string(abi.encodePacked(baseURI, Strings.toString(tokenId)))
+                : "";
+    }
+
+    function withdrawPayments(address payable payee) external onlyOwner {
+        uint256 balance = address(this).balance;
+        (bool transferTx, ) = payee.call{value: balance}("");
+        if (!transferTx) {
+            revert WithdrawTransfer();
+        }
+    }
+}
---
+++ test/Editor.t.sol
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+import "../lib/forge-std/src/Test.sol";
+
+import "src/Editor/Editor.sol";
+
+contract TestEditor is Test {
+    using stdStorage for StdStorage;
+    Vm internal constant hevm = Vm(HEVM_ADDRESS);
+    Editor internal editor;
+
+    function setUp() public {
+        editor = new Editor("Editor NFT", "EDT", "http://example.com/");
+    }
+
+    function testFailNoMintPricePaid() public {
+        editor.mintTo(address(1));
+    }
+
+    function testMintPricePaid() public {
+        editor.mintTo{value: 0.08 ether}(address(1));
+    }
+
+    function testTokenURI() public {
+        editor.mintTo{value: 0.08 ether}(address(1));
+
+        assertEq(editor.tokenURI(1), "http://example.com/1");
+        editor.mintTo{value: 0.08 ether}(address(1));
+
+        assertEq(editor.tokenURI(2), "http://example.com/2");
+    }
+
+    function testFailMaxSupplyReached() public {
+        uint256 slot = stdstore
+            .target(address(editor))
+            .sig("currentTokenId()")
+            .find();
+        bytes32 loc = bytes32(slot);
+        bytes32 mockedCurrentTokenId = bytes32(abi.encode(10000));
+        hevm.store(address(editor), loc, mockedCurrentTokenId);
+        editor.mintTo{value: 0.08 ether}(address(1));
+    }
+
+    function testFailMintToZeroAddress() public {
+        editor.mintTo{value: 0.08 ether}(address(0));
+    }
+
+    function testNewMintOwnerRegistered() public {
+        editor.mintTo{value: 0.08 ether}(address(1));
+        uint256 slotOfNewOwner = stdstore
+            .target(address(editor))
+            .sig(editor.ownerOf.selector)
+            .with_key(1)
+            .find();
+
+        uint160 ownerOfTokenIdOne = uint160(
+            uint256(
+                (
+                    hevm.load(
+                        address(editor),
+                        bytes32(abi.encode(slotOfNewOwner))
+                    )
+                )
+            )
+        );
+        assertEq(address(ownerOfTokenIdOne), address(1));
+    }
+
+    function testBalanceIncremented() public {
+        editor.mintTo{value: 0.08 ether}(address(1));
+        uint256 slotBalance = stdstore
+            .target(address(editor))
+            .sig(editor.balanceOf.selector)
+            .with_key(address(1))
+            .find();
+
+        uint256 balanceFirstMint = uint256(
+            hevm.load(address(editor), bytes32(slotBalance))
+        );
+        assertEq(balanceFirstMint, 1);
+
+        editor.mintTo{value: 0.08 ether}(address(1));
+        uint256 balanceSecondMint = uint256(
+            hevm.load(address(editor), bytes32(slotBalance))
+        );
+        assertEq(balanceSecondMint, 2);
+    }
+
+    function testIncreaseTotalSupply(uint256 amount) public {
+        if (amount > editor.TOTAL_SUPPLY()) return;
+        uint256 prevSupply = editor.TOTAL_SUPPLY();
+        editor.increaseTotalSupply(amount);
+        assertEq(editor.TOTAL_SUPPLY(), prevSupply + amount);
+    }
+
+    function testSafeContractReceiver() public {
+        Receiver receiver = new Receiver();
+        editor.mintTo{value: 0.08 ether}(address(receiver));
+        uint256 slotBalance = stdstore
+            .target(address(editor))
+            .sig(editor.balanceOf.selector)
+            .with_key(address(receiver))
+            .find();
+
+        uint256 balance = uint256(
+            hevm.load(address(editor), bytes32(slotBalance))
+        );
+        assertEq(balance, 1);
+    }
+
+    function testFailUnSafeContractReceiver() public {
+        hevm.etch(address(1), bytes("mock code"));
+        editor.mintTo{value: 0.08 ether}(address(1));
+    }
+
+    function testWithdrawalWorksAsOwner() public {
+        // Mint an NFT, sending eth to the contract
+        Receiver receiver = new Receiver();
+        address payable payee = payable(address(0x1337));
+        uint256 priorPayeeBalance = payee.balance;
+        editor.mintTo{value: editor.PRICE_PER_MINT()}(address(receiver));
+        // Check that the balance of the contract is correct
+        assertEq(address(editor).balance, editor.PRICE_PER_MINT());
+        uint256 nftBalance = address(editor).balance;
+        // Withdraw the balance and assert it was transferred
+        editor.withdrawPayments(payee);
+        assertEq(payee.balance, priorPayeeBalance + nftBalance);
+    }
+
+    function testWithdrawalFailsAsNotOwner() public {
+        // Mint an NFT, sending eth to the contract
+        Receiver receiver = new Receiver();
+        editor.mintTo{value: editor.PRICE_PER_MINT()}(address(receiver));
+        // Check that the balance of the contract is correct
+        assertEq(address(editor).balance, editor.PRICE_PER_MINT());
+        // Confirm that a non-owner cannot withdraw
+        hevm.expectRevert("UNAUTHORIZED");
+        hevm.startPrank(address(0xd3ad));
+        editor.withdrawPayments(payable(address(0xd3ad)));
+        hevm.stopPrank();
+    }
+}
+
+contract Receiver is ERC721TokenReceiver {
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 id,
+        bytes calldata data
+    ) external pure override returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+}
