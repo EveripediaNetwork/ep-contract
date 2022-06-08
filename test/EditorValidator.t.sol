ab3bccc928464c2c62ae8a385258b1a131367b21
Yusuf Kehinde Hussein <yusufkehinde11@gmail.com>
2022-06-08 12:48:26 +0100

    feat: added edit count view function

--- test/EditorValidator.t.sol
+++ test/EditorValidator.t.sol
@@ -17,28 +17,32 @@ contract TestEditorValidator is Test {
 
     function testValidate() public {
         for (uint256 i = 0; i < 5; i++) {
-            editorValidator.validate(
-                editor,
-                "Qmb7Kc2r7oH6ff5VdvV97ynuv9uVNXPVppjiMvkGF98F6v"
-            );
+            editorValidator.validate(editor, "Qmb7Kc2r7oH6ff5VdvV97ynuv9uVNXPVppjiMvkGF98F6v");
         }
         skip(10 hours);
         vm.expectRevert(EditorValidator.ExceededEditLimit.selector);
-        editorValidator.validate(
-            editor,
-            "Qmb7Kc2r7oH6ff5VdvV97ynuv9uVNXPVppjiMvkGF98F6v"
-        );
+        editorValidator.validate(editor, "Qmb7Kc2r7oH6ff5VdvV97ynuv9uVNXPVppjiMvkGF98F6v");
         skip(14 hours);
         for (uint256 i = 0; i < 5; i++) {
-            editorValidator.validate(
-                editor,
-                "Qmb7Kc2r7oH6ff5VdvV97ynuv9uVNXPVppjiMvkGF98F6v"
-            );
+            editorValidator.validate(editor, "Qmb7Kc2r7oH6ff5VdvV97ynuv9uVNXPVppjiMvkGF98F6v");
         }
         vm.expectRevert(EditorValidator.ExceededEditLimit.selector);
-        editorValidator.validate(
-            editor,
-            "Qmb7Kc2r7oH6ff5VdvV97ynuv9uVNXPVppjiMvkGF98F6v"
-        );
+        editorValidator.validate(editor, "Qmb7Kc2r7oH6ff5VdvV97ynuv9uVNXPVppjiMvkGF98F6v");
+    }
+
+    function testGetRemainEditsCount() public {
+        for (uint256 i = 0; i < 3; i++) {
+            editorValidator.validate(editor, "Qmb7Kc2r7oH6ff5VdvV97ynuv9uVNXPVppjiMvkGF98F6v");
+        }
+        skip(10 hours);
+        assertEq(editorValidator.getEditsCount(editor), 2);
+        skip(14 hours);
+        assertEq(editorValidator.getEditsCount(editor), 5);
+        for (uint256 i = 0; i < 5; i++) {
+            editorValidator.validate(editor, "Qmb7Kc2r7oH6ff5VdvV97ynuv9uVNXPVppjiMvkGF98F6v");
+        }
+        assertEq(editorValidator.getEditsCount(editor), 0);
+        skip(1 days);
+        assertEq(editorValidator.getEditsCount(editor), 5);
     }
 }
